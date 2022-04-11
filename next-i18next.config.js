2994a7fbaafc05fb9d3096078c82b25b231ca216
Syed Abdullah <57061302+SyedAbdu11ah@users.noreply.github.com>
2022-04-11 13:25:49 +0500

    Add files via upload

---
+++ next-i18next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    i18n:{
+        locales:["en","de","es","fr","hi","id","it","ja","ko","my","ms","nl","en-PH","pt","ru","th","tr","vi","zh","zt","sa","bn"],
+        defaultLocale: 'en'
+    },
+  }
+  
---
+++ next.config.js
@@ -0,0 +1,7 @@
+const { i18n } = require ("./next-i18next.config");
+
+module.exports = {
+
+  ReactStrictMode: true,
+  i18n,
+};
